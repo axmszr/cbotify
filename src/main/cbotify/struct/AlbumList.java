cbf1ea5c80d55415d2c1d2482bad2aa228c4998c
axmszr <e0727266@u.nus.edu>
2023-07-25 03:29:18 +0800

    Encapsulate constructors

--- src/main/cbotify/struct/AlbumList.java
+++ src/main/cbotify/struct/AlbumList.java
@@ -2,15 +2,35 @@ package cbotify.struct;
 
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 import cbotify.song.Album;
 import cbotify.song.Artist;
 
 class AlbumList {
+    private static HashMap<Album, Album> emptyMap = new HashMap<>();
+
     private HashMap<Album, Album> albums;
-    
-    AlbumList() {
-        this.albums = new HashMap<>();
+
+    AlbumList(Map<Album, Album> albums) {
+        this.albums = new HashMap<>(albums);
+    }
+
+    public static AlbumList makeNew() {
+        return new AlbumList(emptyMap);
+    }
+
+    public static AlbumList makeWith(List<Album> albumList) {
+        Map<Album, Album> albumMap = albumList.stream()
+                .collect(Collectors.toMap(Function.identity(), Function.identity()));
+
+        return new AlbumList(albumMap);
+    }
+
+    public static AlbumList makeCopy(AlbumList albumList) {
+        return new AlbumList(albumList.albums);
     }
     
     public boolean hasAlbum(Album album) {
--- src/main/cbotify/struct/ArtistList.java
+++ src/main/cbotify/struct/ArtistList.java
@@ -1,14 +1,31 @@
 package cbotify.struct;
 
 import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 import cbotify.song.Artist;
 
 class ArtistList {
+    private static HashSet<Artist> emptySet = new HashSet<>();
+
     private HashSet<Artist> artists;
-    
-    ArtistList() {
-        this.artists = new HashSet<>();
+
+    ArtistList(Set<Artist> artists) {
+        this.artists = new HashSet<>(artists);
+    }
+
+    public static ArtistList makeNew() {
+        return new ArtistList(emptySet);
+    }
+
+    public static ArtistList makeWith(List<Artist> artistList) {
+        HashSet<Artist> artistSet = new HashSet<>(artistList);
+        return new ArtistList(artistSet);
+    }
+
+    public static ArtistList makeCopy(ArtistList artistList) {
+        return new ArtistList(artistList.artists);
     }
     
     public boolean hasArtist(Artist artist) {
