fef8ebfc492c3aae7eb27c9a98a710d19fb1cc75
axmszr <e0727266@u.nus.edu>
2023-07-27 05:43:53 +0800

    Add basic adding methods

--- src/main/cbotify/struct/Storage.java
+++ src/main/cbotify/struct/Storage.java
@@ -1,13 +1,20 @@
 package cbotify.struct;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import cbotify.song.Album;
+import cbotify.song.Artist;
+import cbotify.song.Song;
+
 public class Storage {
-    private final AlbumList albumList;
     private final ArtistList artistList;
+    private final AlbumList albumList;
     private final SongList songList;
 
     Storage(AlbumList albumList, ArtistList artistList, SongList songList) {
-        this.albumList = albumList;
         this.artistList = artistList;
+        this.albumList = albumList;
         this.songList = songList;
     }
 
@@ -19,11 +26,31 @@ public class Storage {
         return new Storage(emptyAlbumList, emptyArtistList, emptySongList);
     }
 
-    public Storage makeNewWith(AlbumList newAlbumList, ArtistList newArtistList, SongList newSongList) {
+    public Storage load(AlbumList newAlbumList, ArtistList newArtistList, SongList newSongList) {
         return new Storage(newAlbumList, newArtistList, newSongList);
     }
 
-    // public void addArtist
-    // public void addAlbum
-    // public void addSong
+    public Artist addArtist(String name) {
+        return this.artistList.makeArtist(name);
+    }
+
+    public Album addAlbum(String albumTitle, List<String> artistNames) {
+        List<Artist> artists = new ArrayList<>();
+        artistNames.stream()
+                .map(this::addArtist)
+                .forEachOrdered(artists::add);
+
+        return this.albumList.makeAlbum(albumTitle, artists);
+    }
+
+    public Song addSong(String songTitle, String albumTitle, List<String> albumArtists, List<String> artistNames) {
+        Album album = addAlbum(albumTitle, albumArtists);
+
+        List<Artist> artists = new ArrayList<>();
+        artistNames.stream()
+                .map(this::addArtist)
+                .forEachOrdered(artists::add);
+
+        return this.songList.makeSong(songTitle, album, artists);
+    }
 }
